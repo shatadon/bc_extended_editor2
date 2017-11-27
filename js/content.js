04e680b867e314437284bacc520c3dfcd2ec27cb
qdhenry <qdhenry@gmail.com>
2017-11-27 15:38:57 -0500

    added product ID to the api put request

--- js/content.js
+++ js/content.js
@@ -136,7 +136,8 @@ function cancel_edit() {
 
 function update_product() {
           // define product data
-         var name = jQuery('input#name').prop("value");
+          productID = jQuery('input[type="hidden"][name="product_id"]').val();
+          var name = jQuery('input#name').prop("value");
           var sku = jQuery('input#sku').prop("value");
           var price = jQuery('input#price').prop("value");
           var retail_price = jQuery('input#retail_price').prop("value");
@@ -157,13 +158,12 @@ xhr.withCredentials = true;
 
 xhr.addEventListener("readystatechange", function () {
   if (this.readyState === 4) {
-    // console.log(this.responseText);
     location.reload();
   }
 
 });
 
-xhr.open("PUT", "https://api.bigcommerce.com/stores/8316qn/v3/catalog/products/2");
+xhr.open("PUT", "https://api.bigcommerce.com/stores/8316qn/v3/catalog/products/" + productID );
 xhr.setRequestHeader("x-auth-client", "pkdj66fxj2obi15e794gwa31vgt7j1c");
 xhr.setRequestHeader("x-auth-token", "at7y5ayx1vx3b7yrc87n2ecqo24phwc");
 xhr.setRequestHeader("content-type", "application/json");
@@ -171,113 +171,6 @@ xhr.setRequestHeader("cache-control", "no-cache");
 xhr.setRequestHeader("postman-token", "b1f6a5b4-0940-3623-9ac3-b0357287bc88");
 
 xhr.send(data);
-
-//     chrome.storage.sync.get({
-//       'client_ID':'',
-//       'auth_Token':'',
-//   		'store_Hash':''
-//     }, function(items) {
-//       var productID = jQuery('input[type="hidden"][name="product_id"]').val();
-//       var apiURL = "https://api.bigcommerce.com/stores/" + items.store_Hash + "/v3/catalog/products/" + productID;
-//
-//       // define product data
-//      var name = jQuery('input#name').prop("value");
-//       var sku = jQuery('input#sku').prop("value");
-//       var price = jQuery('input#price').prop("value");
-//       var retail_price = jQuery('input#retail_price').prop("value");
-//       var sale_price = jQuery('input#sale_price').prop("value");
-//       var desc = jQuery('textarea#desc').html();
-//       var type = jQuery('select#type option:selected').prop("value");
-//       var weight = jQuery('input#weight').prop("value");
-//       var height = jQuery('input#height').prop("value");
-//       var width = jQuery('input#width').prop("value");
-//       var depth = jQuery('input#depth').prop("value");
-//       var isVisible = jQuery('select#isVisible option:selected').prop("value");
-//       var isFeatured = jQuery('select#isFeatured option:selected').prop("value");
-//       var warranty = jQuery('textarea#warranty').html();
-//       var bin = jQuery('input#bin').prop("value");
-//       var upc = jQuery('input#upc').prop("value");
-//       var availability = jQuery('select#availability option:selected').prop("value");
-//       var avail_desc = jQuery('input#avail_desc').prop("value");
-//       var showCondition = jQuery('select#showCondition option:selected').prop("value");
-//       var condition = jQuery('select#condition option:selected').prop("value");
-//       var min_qty = jQuery('input#min_qty').prop("value");
-//       var max_qty = jQuery('input#max_qty').prop("value");
-//       var invTracking = jQuery('select#invTracking option:selected').prop("value");
-//       var inv_Warning = jQuery('input#inv_Warning').prop("value");
-//       var inv_Level = jQuery('input#inv_Level').prop("value");
-//       var settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "https://api.bigcommerce.com/stores/8316qn/v3/catalog/products/2",
-//   "method": "PUT",
-//   "headers": {
-//     "x-auth-client": "pkdj66fxj2obi15e794gwa31vgt7j1c",
-//     "x-auth-token": "at7y5ayx1vx3b7yrc87n2ecqo24phwc",
-//     "cache-control": "no-cache",
-//     "postman-token": "cd1e3308-1433-bc63-f77f-e2805eaabc95"
-//   },
-//
-// }
-//
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
-//       var settings = {
-//         "async": true,
-//         "crossDomain": true,
-//         "url": apiURL,
-//         "context": document.body,
-//         "method": "PUT",
-//         "headers": {
-//             "x-auth-token": items.auth_Token,
-//             "x-auth-client": items.client_ID,
-//             "cache-control": "no-cache"
-//           },
-//         "body": {
-//             "name": name,
-//             "type": type,
-//             "sku": sku,
-//             "description": desc,
-//             "weight": weight,
-//             "width": width,
-//             "depth": depth,
-//             "height": height,
-//             "price": price,
-//             "retail_price": retail_price,
-//             "sale_price": sale_price,
-//             "inventory_level": inv_Level,
-//             "inventory_warning_level": inv_Warning,
-//             "inventory_tracking": invTracking,
-//             "is_visible": isVisible,
-//             "is_featured": isFeatured,
-//             "warranty": warranty,
-//             "bin_picking_number": bin,
-//             "upc": upc,
-//             "availability": availability,
-//             "availability_description": avail_desc,
-//             "condition": condition,
-//             "is_condition_shown": showCondition,
-//             "order_quantity_minimum": min_qty,
-//             "order_quantity_maximum": max_qty
-//           }
-//
-//       }; //end settings variable
-//       $.ajax(settings).done(function (response) {
-//           console.log(name);
-//           console.log(apiURL)
-//
-//             console.log("Success: " + response.data);
-//             var updateStatus = jQuery('.modal-body.container');
-//             jQuery(updateStatus).html("<h1 style='color:green;'>Product Updated Successfully!</h1>");
-//             setTimeout(function() {
-//               // cancel_edit();
-//             }, 750);
-//       }).fail(function (response) {
-//             console.log("Fail Reason: " + response.error);
-//       }); //end response ajax
-//
-//     }); // end Storage get function
 
 } // end update product function
 

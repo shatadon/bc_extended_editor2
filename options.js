cf50604b8e705fafb6fddce3e3573aa8aade811b
qdhenry <qdhenry@gmail.com>
2017-11-29 16:28:59 -0500

    solves 17 test

--- options.js
+++ options.js
@@ -19,7 +19,7 @@ function save_options() {
   });
 }
 
-
+// testing pull request linkage to task inside of  ZenHub
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
